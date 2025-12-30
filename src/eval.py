b5ae3efbfeb6804e49b313d91791ba23e2d99076
Kidus yoseph <kidusy567@gmail.com>
2025-12-30 18:14:48 +0300

    fix GridSearchCV best_params_ access

--- src/eval.py
+++ src/eval.py
@@ -8,7 +8,6 @@ def evaluate(model,X_test,y_test):
     precision_rf, recall_rf, _ = precision_recall_curve(y_test, rf_probs)
     auc_pr_rf = auc(recall_rf, precision_rf)
 
-    print(f"Best Parameters: {model.best_params_}")
     print(f"Random Forest AUC-PR: {auc_pr_rf:.4f}")
     print("\nRandom Forest Classification Report:")
     print(classification_report(y_test, rf_preds))
