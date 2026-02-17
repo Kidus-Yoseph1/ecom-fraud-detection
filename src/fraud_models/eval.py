331b3459107db92d8921c8ed60d9b62888d2f098
Kidus yoseph <kidusy567@gmail.com>
2026-02-17 10:13:43 +0300

    evaluation for rf model

---
+++ src/fraud_models/eval.py
@@ -0,0 +1,18 @@
+from sklearn.metrics import (classification_report, confusion_matrix,
+                              precision_recall_curve, auc)
+
+class Evaluate_models():
+
+    def evaluate_rf(model,X_test,y_test):
+        rf_preds = model.predict(X_test)
+        rf_probs = model.predict_proba(X_test)[:, 1]
+
+        precision_rf, recall_rf, _ = precision_recall_curve(y_test, rf_probs)
+        auc_pr_rf = auc(recall_rf, precision_rf)
+
+        print(f"Random Forest AUC-PR: {auc_pr_rf:.4f}")
+        print("\nRandom Forest Classification Report:")
+        print(classification_report(y_test, rf_preds))
+    
+    def evaluate_autoencoder():
+        pass
